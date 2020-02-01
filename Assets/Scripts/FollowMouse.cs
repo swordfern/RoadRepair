eacc361e6d25813cf45395a61f9569ecbffca7b8
Nicky <swordferngames@gmail.com>
2020-02-01 13:45:00 -0800

    Adds configurable z position to follow mouse

--- Assets/Scripts/FollowMouse.cs
+++ Assets/Scripts/FollowMouse.cs
@@ -2,18 +2,19 @@
 
 public class FollowMouse : MonoBehaviour
 {
-    [SerializeField] Camera _camera;
+    [SerializeField] private Camera _camera;
+    [SerializeField] private float _zPosition;
 
     private Transform _cachedTransform;
 
     private void Awake()
     {
-        _cachedTransform = transform;   
+        _cachedTransform = transform;  
     }
 
     void Update()
     {
         var mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
-        _cachedTransform.localPosition = mousePosition;
+        _cachedTransform.localPosition = new Vector3(mousePosition.x, mousePosition.y, _zPosition);
     }
 }
