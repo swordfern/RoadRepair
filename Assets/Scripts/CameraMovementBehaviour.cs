da6240329ae7cd2979c1eeee70f3c1925684ab44
Nicky <swordferngames@gmail.com>
2020-01-31 23:21:56 -0800

    Adds basic camera movement and main scene

---
+++ Assets/Scripts/CameraMovementBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementBehaviour : MonoBehaviour
+{
+    [SerializeField] private Vector3 _velocityPerSecond;
+
+    private Transform _cachedTransform;
+
+    private void Awake()
+    {
+        _cachedTransform = transform;
+    }
+
+    private void Update()
+    {
+        var delta = Time.deltaTime;
+        var positionDelta = _velocityPerSecond * delta;
+        _cachedTransform.localPosition += positionDelta;
+    }
+}
