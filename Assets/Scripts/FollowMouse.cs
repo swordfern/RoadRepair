a30218084871743d273eb03a80a7e5faf7349493
Nicky <swordferngames@gmail.com>
2020-02-01 13:35:02 -0800

    Adds follow mouse script

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    [SerializeField] Camera _camera;
+
+    private Transform _cachedTransform;
+
+    private void Awake()
+    {
+        _cachedTransform = transform;   
+    }
+
+    void Update()
+    {
+        var mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        _cachedTransform.localPosition = mousePosition;
+    }
+}
