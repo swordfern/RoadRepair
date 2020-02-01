6f9ef3faac01b084f83b2f12fe72e125fe2af2ae
Nicky <swordferngames@gmail.com>
2020-02-01 00:00:37 -0800

    Added car health

---
+++ Assets/CarHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarHealth : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth;
+
+    private int _health;
+
+    private void Awake()
+    {
+        ResetHealth();
+    }
+
+    private void ResetHealth()
+    {
+        _health = _maxHealth;
+    }
+
+    public bool ApplyDamageAndTryDestroy(int damage)
+    {
+        _health -= damage;
+        return _health <= 0;
+    }
+}
