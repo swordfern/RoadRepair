f811ac143e07a6ed413ec5fd6bb68f993d0fe430
dumcreator <dumcreator@users.noreply.github.com>
2020-02-01 18:25:22 -0800

    Art Update
    Just addin art

--- Assets/Scripts/MovableItem.cs
+++ Assets/Scripts/MovableItem.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using static MovableTargetPairs;
 
-public class MovableItem : MonoBehaviour, IInputTarget
+public class MovableItem : MonoBehaviour
 {
     [SerializeField] private MovableType _type;
     public MovableType MovableType => _type;    
--- Assets/Scripts/MovableMover.cs
+++ Assets/Scripts/MovableMover.cs
@@ -4,37 +4,27 @@ public class MovableMover : MonoBehaviour
 {
     [SerializeField] private MovableTargetPairs _movableTargetPairs;
 
-    private Transform _cachedTransform;
     private MovableItem _heldItem;
-
-    private void Awake()
-    {
-        _cachedTransform = transform;
-    }
 
     public void PickeupMovable(MovableItem item)
     {
-        if (_heldItem == null)
-        {
-            _heldItem = item;
-            _heldItem.transform.SetParent(_cachedTransform);
-        }
+        _heldItem = item;
     }
 
-    public void PlaceMovable(TargetObject targetObject)
+    public bool TryPlaceMovable(TargetObject targetObject)
     {
-        if (_heldItem == null)
+        if (_heldItem == null || targetObject.HasPlacedItem())
         {
-            return;
+            return false;
         }
 
         if (_movableTargetPairs.IsMatch(_heldItem.MovableType, targetObject.TargetType))
         {
-            var placedMovable = targetObject.TryPlaceMovable(_heldItem);
-            if (placedMovable)
-            {
-                _heldItem = null;
-            }
+            targetObject.PlaceMovable(_heldItem);
+            _heldItem = null;
+            return true;
         }
+
+        return false;
     }
 }
