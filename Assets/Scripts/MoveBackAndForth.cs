279a532ee658cf8a572ebbf2be774e28ec64b1f3
Nicky <swordferngames@gmail.com>
2020-02-02 13:26:19 -0800

    Makes move back and forth disableable

--- Assets/Scripts/MoveBackAndForth.cs
+++ Assets/Scripts/MoveBackAndForth.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class MoveBackAndForth : MonoBehaviour
+public class MoveBackAndForth : MonoBehaviour, ITemporarilyDisableable
 {
     [SerializeField] private float _xMin;
     [SerializeField] private float _xMax;
@@ -10,6 +10,8 @@ public class MoveBackAndForth : MonoBehaviour
     private float _elapsedTime;
     private float _startX;
     private float _targetX;
+
+    private bool _isDisabled;
 
     private void Awake()
     {
@@ -22,6 +24,11 @@ public class MoveBackAndForth : MonoBehaviour
 
     private void Update()
     {
+        if (_isDisabled)
+        {
+            return;
+        }
+
         var prevPos = _cachedTransform.localPosition;
         var newElapsedTime = _elapsedTime + Time.deltaTime;
         _elapsedTime = Mathf.Min(newElapsedTime, _moveTime);
@@ -43,5 +50,10 @@ public class MoveBackAndForth : MonoBehaviour
         _elapsedTime = 0;
         var prevScale = _cachedTransform.localScale;
         _cachedTransform.localScale = new Vector3(-1 * prevScale.x, prevScale.y, prevScale.z);
+    }
+
+    public void SetTemporarilyDisabled(bool isDisabled)
+    {
+        _isDisabled = isDisabled;
     }
 }
