3cb28be48e4f1b3d5b94bf8f0351f89a490aeeed
Nicky <swordferngames@gmail.com>
2020-02-01 23:32:04 -0800

    Adds random movement to car

--- Assets/Scripts/CarMovementBehaviour.cs
+++ Assets/Scripts/CarMovementBehaviour.cs
@@ -2,9 +2,14 @@
 
 public class CarMovementBehaviour : MonoBehaviour
 {
-    [SerializeField] private Vector3 _velocityPerSecond;
+    [SerializeField] private float _yVelocity;
+    [Header("Lateral Movement")]
+    [SerializeField] private float _amplitude;
+    [SerializeField] private float _frequency;
+    [SerializeField] private float _spread;
 
     private Transform _cachedTransform;
+    private float _elapsedTime;
 
     private void Awake()
     {
@@ -13,8 +18,19 @@ public class CarMovementBehaviour : MonoBehaviour
 
     private void Update()
     {
+        var prevPos = _cachedTransform.localPosition;
+
         var delta = Time.deltaTime;
-        var positionDelta = _velocityPerSecond * delta;
-        _cachedTransform.localPosition += positionDelta;
+        _elapsedTime += delta;
+        var yDelta = _yVelocity * delta;
+        var y = yDelta + prevPos.y;
+        var x = GetX();
+        _cachedTransform.localPosition = new Vector3(x, y, prevPos.z);        
+    }
+
+    private float GetX()
+    {       
+        // Psuedo-random wave that starts slow and gets faster
+        return _amplitude * Mathf.Sin(_frequency * Mathf.Sin(_spread* Mathf.Sin(_elapsedTime) * _elapsedTime) *_elapsedTime);
     }
 }
